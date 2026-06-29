4dd604948b568817a6fbc1c5b8c5ebc76c21db83
byzin <bayzin02@gmail.com>
2026-06-28 23:52:26 -0300

    Remove setpixel function and its implementation

--- setpixel.py
+++ setpixel.py
@@ -1,17 +1,1 @@
-import win32gui as gui
-import win32api as api
-import time
-
-def setpixel():
-    sw = api.GetSystemMetrics(0)
-    sh = api.GetSystemMetrics(1)
-    while True:
-        hdc = gui.GetDC(0)
-
-        for y in range(sh):
-            gui.PatBlt(hdc, 0, y, 0, sh, )
-
-        gui.ReleaseDC(0, hdc)
-
-time.sleep(1)
-setpixel()
+# 
